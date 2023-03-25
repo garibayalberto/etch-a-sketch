543e2679e0aff28ae5a4f6f1784487dcb2b591c7
Alberto <garibayalberto98@gmail.com>
2023-03-25 09:41:25 -0700

    JS functionality completed

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ let cells = Array.from(document.getElementsByClassName('cell-item'))
 // values
 let currColor = document.getElementById("colorPicker").value;
 let randModeOn = document.getElementById("randomMode").value;
-let lightBrushOn = document.getElementById("lightBrush").value;
 
 const createRandHex = () => {
   const randomColor = Math.floor(Math.random()*16777215).toString(16);
@@ -24,12 +23,6 @@ function createGrid(size){
   for (let i = 0; i < size * size ; i++) {
     
     let cell = document.createElement("div");
-
-    //based on the grid dimensions - calc the cell width
-    let cellSize = ( (size / (size*size)) * 100);
-
-    //cell.style.width = cellSize + "%";
-    //cell.style.paddingBottom = cellSize + "%";
 
     gridContainer.appendChild(cell).className = "cell-item";
 
@@ -49,29 +42,11 @@ function setSizeText(size) {
   colsText.textContent=size;
 }
 
-function lightBrushEffect(cellItem) {
-  let currOpacity = cellItem.style.opacity.value;
-
-  console.log(cellItem.style.opacity.value);
-  if( cellItem.style.backgroundColor.value === currColor && 
-    cellItem.currOpacity === 1){
-    return;
-  }
-  else{
-    cellItem.style.opacity = currOpacity + .1;
-  }
-  
-}
-
 //Fill in color
 function fillColor() {
   cells = Array.from(document.getElementsByClassName('cell-item'));
   cells.forEach(cell => {
     cell.addEventListener('mouseover', function handleHover(event) {
-        //console.log(randModeOn);
-        //console.log(lightBrushOn);
-
-        cell.style.opacity = 1;
         
         if(randModeOn == "false" || randModeOn == false){
           cell.style.backgroundColor = currColor;
@@ -79,11 +54,6 @@ function fillColor() {
         else if (randModeOn == true) {
           let randColor = createRandHex();
           cell.style.backgroundColor = "#" + randColor;
-        }
-  
-        if(lightBrushOn == true){
-          console.log(cell.style.opacity.value);
-          lightBrushEffect(cell);
         }
     });
   });
@@ -106,7 +76,6 @@ newGridSize.addEventListener('input', function() {
   createGrid(newSize);
   setSizeText(newSize);
 
-  //cells = Array.from(document.getElementsByClassName('cell-item'));
   fillColor();
 });
 
@@ -130,11 +99,4 @@ const el_randomMode = document.getElementById("randomMode");
 el_randomMode.addEventListener('input', function() {
   randModeOn = el_randomMode.checked;
   //console.log(randModeOn);
-});
-
-//Light Brush On/Off 
-const el_lightBrush = document.getElementById("lightBrush");
-el_lightBrush.addEventListener('input', function() {
-  lightBrushOn = el_lightBrush.checked;
-  console.log(lightBrushOn);
 });
