5024f5636b4717efe3f7f27a567c58af944454ad
Alberto <garibayalberto98@gmail.com>
2023-03-27 19:41:13 -0700

    Finalized code , new HTML , CSS , JS , removed old JS backup

--- script.js
+++ script.js
@@ -9,13 +9,18 @@ const rowsText = document.getElementById("rows");
 const colsText = document.getElementById("cols");
 let cells = Array.from(document.getElementsByClassName('cell-item'))
 
+const el_colorMode = document.getElementById("colorMode");
+const el_randomMode = document.getElementById("randomMode");
+let coloModeOn = true;
+el_colorMode.classList.add("active");
+let randModeOn = false;
+
 // values
 let currColor = document.getElementById("colorPicker").value;
-let randModeOn = document.getElementById("randomMode").value;
 
 const createRandHex = () => {
   const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  return randomColor;
+  return "#" + randomColor;
 }
 
 function createGrid(size){  
@@ -53,7 +58,7 @@ function fillColor() {
         }
         else if (randModeOn == true) {
           let randColor = createRandHex();
-          cell.style.backgroundColor = "#" + randColor;
+          cell.style.backgroundColor = randColor;
         }
     });
   });
@@ -66,17 +71,28 @@ function main() {
 
 main();
 
-//Generate new grid
-const newGridSize = document.getElementById("gridSize");
-newGridSize.addEventListener('input', function() {
+//Color Change
+const el_colorPicker = document.getElementById("colorPicker");
+el_colorPicker.addEventListener('input', function() {
+  currColor = el_colorPicker.value;
+});
 
-  deleteGrid();
+//Color Mode On/Off 
+el_colorMode.addEventListener('click', function() {
+  //el_colorMode.focus();
+  el_colorMode.classList.add("active");
+  el_randomMode.classList.remove("active");
+  colorModeOn = true;
+  randModeOn = false;
+});
 
-  let newSize = parseFloat(newGridSize.value)
-  createGrid(newSize);
-  setSizeText(newSize);
-
-  fillColor();
+//Random Mode On/Off 
+el_randomMode.addEventListener('click', function() {
+  //el_randomMode.focus();
+  el_randomMode.classList.add("active");
+  el_colorMode.classList.remove("active");
+  randModeOn = true;
+  colorModeOn = false;
 });
 
 //Clear Grid
@@ -88,15 +104,15 @@ clearGrid.addEventListener('click', function() {
   });
 });
 
-//Color Change
-const el_colorPicker = document.getElementById("colorPicker");
-el_colorPicker.addEventListener('input', function() {
-  currColor = el_colorPicker.value;
-});
+//Generate new grid
+const newGridSize = document.getElementById("gridSize");
+newGridSize.addEventListener('input', function() {
 
-//Random Mode On/Off 
-const el_randomMode = document.getElementById("randomMode");
-el_randomMode.addEventListener('input', function() {
-  randModeOn = el_randomMode.checked;
-  //console.log(randModeOn);
+  deleteGrid();
+
+  let newSize = parseFloat(newGridSize.value)
+  createGrid(newSize);
+  setSizeText(newSize);
+
+  fillColor();
 });
